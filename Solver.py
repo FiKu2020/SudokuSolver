cc4c4225f71339f34fb3e0948d92390defd2dc81
filip k <filip2006kujawa@gmail.com>
2024-04-04 19:55:03 +0100

    nienawidze nazywac wartosci po polsku

--- Solver.py
+++ Solver.py
@@ -13,10 +13,13 @@ TablicaNr1 = [
 
 def is_format_correct():
     pass
-def is_number_in_row(tablica,rzad,kolumna,cyfra):
+def is_number_in_row(tablica,row,collum,number):
     pass
 def find_empty_space(tablica):
-    pass
+    for row in tablica:
+        for collum in tablica:
+            if tablica[row][collum] == "_" :
+                return ( row,collum)
 def solve_sudoku():
     pass
 def print_sudoku_board():
