600cf2e568793f9876870424256c591bce87ad18
filip k <filip2006kujawa@gmail.com>
2024-04-04 19:42:54 +0100

    tworz funkcji

--- Solver.py
+++ Solver.py
@@ -1,4 +1,4 @@
-tablica = [
+TablicaNr1 = [
     [_,_,_,_,_,_,_,_,_],
     [_,_,_,_,_,_,_,_,_],
     [_,_,_,_,_,_,_,_,_],
@@ -10,3 +10,14 @@ tablica = [
     [_,_,_,_,_,_,_,_,_],
     [_,_,_,_,_,_,_,_,_]
 ]
+
+def is_format_correct():
+    pass
+def is_number_in_row(tablica,rzad,kolumna,cyfra):
+    pass
+def find_empty_space(tablica):
+    pass
+def solve_sudoku():
+    pass
+def print_sudoku_board():
+    pass
