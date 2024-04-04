c922dbfe4214fd8435a6d2c8f660bd76436cd7ad
filip k <filip2006kujawa@gmail.com>
2024-04-04 22:36:14 +0100

    ost commit 04 04 2024

--- Solver.py
+++ Solver.py
@@ -27,13 +27,23 @@ class Sudoku_Solver(self):
             else:
                     return False
     def is_number_in_collum(tablica,row,collum,number):
-         pass #zrobic czy wartosc jest w kolumnie
+         for i in range(9): #zrobic czy wartosc jest w kolumnie
+               if tablica[i][collum] == number:
+                    return True
+               else:
+                    return False
     def find_empty_space(tablica):
         for row in tablica:
             for collum in tablica:
                 if tablica[row][collum] == 0 :
                     return ( row,collum)
-    def solve_sudoku():
-        pass
+                else:
+                     return False
+    def solve_sudoku(tablica):
+        spaces_to_replace = Sudoku_Solver.find_empty_space(tablica)
+        if spaces_to_replace == False:
+             return True
+        row , collum = spaces_to_replace
+        
     def print_sudoku_board(tablica):
         print(tablica)
