d753dc06049b2bcbb1e8c054796a0bc5108a265e
filip k <filip2006kujawa@gmail.com>
2024-04-04 20:41:18 +0100

    dalej

--- Solver.py
+++ Solver.py
@@ -11,16 +11,25 @@ TablicaNr1 = [
     [0,0,0,0,0,0,0,0,0]
 ]
 
-def is_format_correct():
-    pass
+def is_format_correct(tablica):
+    if len(tablica) !=9:
+        return
+    for row in tablica:
+        pass
+    
 def is_number_in_row(tablica,row,collum,number):
-    pass
+    for i in range(9):
+        pass
+      #      return True
+      #  else:
+     ##       return False
+    #
 def find_empty_space(tablica):
     for row in tablica:
         for collum in tablica:
-            if tablica[row][collum] == "0" :
+            if tablica[row][collum] == 0 :
                 return ( row,collum)
 def solve_sudoku():
     pass
 def print_sudoku_board(tablica):
-    
+    print(tablica)
